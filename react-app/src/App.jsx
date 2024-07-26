b61ba4dbe5aa5014505974977471ffc6cb151109
isaiahL88 <isaiah.ib@outlook.com>
2024-07-26 19:34:33 -0400

    Tailwind, also built out basic elements for react app

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,30 +5,23 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [file, setfile] = useState();
+
+  const handleSubmit = async () => {
+
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col justify-center items-center bg-slate-500 w-screen h-screen">
+      <h1 className='font-mono text-4xl'>Pixelator</h1>
+      <div className="flex flex-col justify-center items-centerw-2/3 bg-slate-300 h-60 z-29">
+        <input onChange={(event) => setfile(event.target.files[0])} type="file" accept="image/*"></input>
+        <button className="btn btn-blue" type='submit' onClick={handleSubmit}>Pixlate Image</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
